a35e5f8e6a7d794b16dcc4161fdd0f7d07b71449
Jethro Muller <git@jethromuller.co.za>
2021-06-16 01:19:39 +0200

    Add input processing code for new change map input

---
+++ src/utils/inputs.ts
@@ -0,0 +1,34 @@
+import * as core from '@actions/core'
+
+type ConfigArray = [string, boolean?]
+type ChangeMap = [string, ConfigArray][]
+
+interface Inputs {
+  branchName: string
+  changeMap: ChangeMap
+}
+
+async function getConfigInput(
+  name: string,
+  options?: core.InputOptions
+): Promise<ChangeMap> {
+  return core
+    .getInput(name, options)
+    .split('\n')
+    .map(s => s.trim())
+    .filter(x => x !== '')
+    .map(value => {
+      const [fileType, config] = value.split(':').map(s => s.trim())
+      return [fileType, JSON.parse(config)]
+    })
+}
+
+export async function getInputs(): Promise<Inputs> {
+  const baseBranchName = core.getInput('base branch')
+  core.debug(`Base Branch Name - ${baseBranchName}`)
+
+  const changeMap = await getConfigInput('change map')
+  core.debug(`Change Map - ${changeMap}`)
+
+  return {branchName: baseBranchName, changeMap}
+}
