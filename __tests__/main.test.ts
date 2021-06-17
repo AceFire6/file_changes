1040468fb6898311832844f41f1e4bb91e14e167
Jethro Muller <git@jethromuller.co.za>
2021-06-17 02:23:21 +0200

    Fix main.test.ts

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,28 +1,24 @@
-import {wait} from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
-
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
 
 // shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
+test('test runs does not error', () => {
+  process.env['INPUT_BASE-BRANCH'] = 'test_changes'
+  process.env['INPUT_FILTER-PATTERNS'] = '{"ADDED":"A\\t","CHANGED":"M\\t","DELETED":"D\\t"}'
+  process.env['INPUT_COMMAND'] = 'cat ./__tests__/{branchName}.txt | grep \'{glob}\''
+  process.env['INPUT_CHANGE-MAP'] = `
+  png: {"glob": ".png"}
+  txt: {"glob": ".txt", "separateDeleted": true}
+  `
+
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecFileSyncOptions = {
-    env: process.env
+    env: process.env,
   }
+
+  console.log(`Running ${np} - ${ip} - ${Object.entries(options)}`)
   console.log(cp.execFileSync(np, [ip], options).toString())
 })
