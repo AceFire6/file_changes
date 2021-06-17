813b84b6f4e29db0bb157bd2ae384a4ba1f326ba
Jethro Muller <git@jethromuller.co.za>
2021-06-18 00:08:34 +0200

    More main test cleanup

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -6,8 +6,12 @@ import * as fs from 'fs'
 
 describe('test main action', () => {
   let tempTestFileName: string
+  const nodePath = process.execPath
+  const actionPath = path.join(__dirname, '..', 'lib', 'main.js')
+  const options: cp.ExecFileSyncOptions = {env: process.env}
 
   beforeAll(() => {
+    // Create temp file
     const {name} = tmp.fileSync()
     const expected_git_changes = `
       A\tadded_text.txt
@@ -16,12 +20,11 @@ describe('test main action', () => {
       A\tadded_img.png
       M\tchanged_img.png
     `
+    // Write expected changes to temp file for subsequent tests
     fs.writeFileSync(name, expected_git_changes)
     tempTestFileName = name
-  })
 
-  // shows how the runner will run a javascript action with env / stdout protocol
-  test('test runs does not error', () => {
+    // Set up environment variables
     process.env['INPUT_BASE-BRANCH'] = tempTestFileName
     process.env['INPUT_FILTER-PATTERNS'] =
       '{"ADDED":"A\\t","CHANGED":"M\\t","DELETED":"D\\t"}'
@@ -30,14 +33,11 @@ describe('test main action', () => {
       png: {"glob": ".png"}
       txt: {"glob": ".txt", "separateDeleted": true}
     `
+  })
 
-    const np = process.execPath
-    const ip = path.join(__dirname, '..', 'lib', 'main.js')
-    const options: cp.ExecFileSyncOptions = {
-      env: process.env
-    }
-
-    const result = cp.execFileSync(np, [ip], options).toString()
+  // shows how the runner will run a javascript action with env / stdout protocol
+  test('test runs does not error', () => {
+    const result = cp.execFileSync(nodePath, [actionPath], options).toString()
 
     const expectedPngOutput = [
       '::set-output name=png::added_img.png changed_img.png',
