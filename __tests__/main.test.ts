5a35b7ae1878aed6d6452116d10440567909a40d
Jethro Muller <git@jethromuller.co.za>
2021-06-17 23:59:59 +0200

    Use tmp to create temp file for main tests instead of hardcoded file

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,38 +1,57 @@
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
+import tmp from 'tmp'
+import * as fs from 'fs'
 
+describe('test main action', () => {
+  let tempTestFileName: string
 
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs does not error', () => {
-  process.env['INPUT_BASE-BRANCH'] = 'test_changes'
-  process.env['INPUT_FILTER-PATTERNS'] = '{"ADDED":"A\\t","CHANGED":"M\\t","DELETED":"D\\t"}'
-  process.env['INPUT_COMMAND'] = 'cat ./__tests__/{branchName}.txt | grep \'{glob}\''
-  process.env['INPUT_CHANGE-MAP'] = `
-  png: {"glob": ".png"}
-  txt: {"glob": ".txt", "separateDeleted": true}
-  `
+  beforeAll(() => {
+    const {name} = tmp.fileSync()
+    const expected_git_changes = `
+      A\tadded_text.txt
+      M\tchanged_text.txt
+      D\tdeleted_text.txt
+      A\tadded_img.png
+      M\tchanged_img.png
+    `
+    fs.writeFileSync(name, expected_git_changes)
+    tempTestFileName = name
+  })
 
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env,
-  }
+  // shows how the runner will run a javascript action with env / stdout protocol
+  test('test runs does not error', () => {
+    process.env['INPUT_BASE-BRANCH'] = tempTestFileName
+    process.env['INPUT_FILTER-PATTERNS'] =
+      '{"ADDED":"A\\t","CHANGED":"M\\t","DELETED":"D\\t"}'
+    process.env['INPUT_COMMAND'] = "cat {branchName} | grep '{glob}'"
+    process.env['INPUT_CHANGE-MAP'] = `
+      png: {"glob": ".png"}
+      txt: {"glob": ".txt", "separateDeleted": true}
+    `
 
-  const result = cp.execFileSync(np, [ip], options).toString()
+    const np = process.execPath
+    const ip = path.join(__dirname, '..', 'lib', 'main.js')
+    const options: cp.ExecFileSyncOptions = {
+      env: process.env
+    }
 
-  const expectedPngOutput = [
-    '::set-output name=png::added_img.png changed_img.png',
-    '::set-output name=any-png::true'
-  ].join('\n')
+    const result = cp.execFileSync(np, [ip], options).toString()
 
-  const expectedTxtOutput = [
-    '::set-output name=deleted-txt::deleted_text.txt',
-    '::set-output name=txt::added_text.txt changed_text.txt',
-    '::set-output name=any-txt::true',
-    '::set-output name=any-matches::true',
-  ].join('\n')
+    const expectedPngOutput = [
+      '::set-output name=png::added_img.png changed_img.png',
+      '::set-output name=any-png::true'
+    ].join('\n')
 
-  expect(result).toContain(expectedPngOutput)
-  expect(result).toContain(expectedTxtOutput)
+    const expectedTxtOutput = [
+      '::set-output name=deleted-txt::deleted_text.txt',
+      '::set-output name=txt::added_text.txt changed_text.txt',
+      '::set-output name=any-txt::true',
+      '::set-output name=any-matches::true'
+    ].join('\n')
+
+    expect(result).toContain(expectedPngOutput)
+    expect(result).toContain(expectedTxtOutput)
+  })
 })
