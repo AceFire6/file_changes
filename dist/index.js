d4cd93a38f9c4297bb4b707637e4cbcc6a72f46c
Jethro Muller <git@jethromuller.co.za>
2021-06-18 03:34:41 +0200

    Package

--- dist/index.js
+++ dist/index.js
@@ -46,7 +46,7 @@ exports.GitChange = {
 function getFileChangesWithCommand(command) {
     return __awaiter(this, void 0, void 0, function* () {
         const { exitCode, stdout, stderr } = yield exec_1.getExecOutput(`/bin/bash -c "${command}"`);
-        core.debug(`stdout = ${stdout} and stderr = ${stderr}`);
+        core.debug(`Command result - stdout = ${stdout} and stderr = ${stderr}`);
         if (exitCode !== 0 || stderr !== '') {
             throw new Error(`Failed to get files - Exit Code ${exitCode} - ${stderr}`);
         }
@@ -129,12 +129,21 @@ function run() {
             for (const { label, config: { glob, separateDeleted }, } of changeMap) {
                 // Generate command to get files for current glob
                 const fileChangeCommand = fileChangeFindCommand.replace('{glob}', glob);
+                core.debug(`Generate file change command - ${fileChangeFindCommand}`);
                 // Get files for glob
                 const fileChanges = yield file_changes_1.getFileChangesWithCommand(fileChangeCommand);
+                core.debug(`File changes - ${fileChanges}`);
                 // Parse fileChanges into list of tuples with ChangeType and filtered name
                 const filteredChanges = file_changes_1.getFilteredChangeMap(fileChanges, filterPatterns);
+                const changesAsStr = Object.entries(filteredChanges)
+                    .map(s => s.join(':'))
+                    .join(',');
+                core.debug(`Filtered changes - ${changesAsStr}`);
                 // Group the file list into ADDED, CHANGED, and DELETE files
                 const { ADDED: addedFiles, CHANGED: changedFiles, DELETED: deletedFiles, } = yield file_changes_1.parseFileChanges(filteredChanges);
+                core.debug(`Parsed changes - ADDED - ${addedFiles}`);
+                core.debug(`Parsed changes - CHANGED - ${changedFiles}`);
+                core.debug(`Parsed changes - DELETED - ${deletedFiles}`);
                 // Group ADDED & CHANGED - these files can still be operated on directly
                 let existingFileChanges = addedFiles.concat(changedFiles);
                 // Check if there are any changes in ADDED, CHANGED, or DELETED
@@ -246,10 +255,13 @@ function getInputs() {
             required: false,
         });
         const filterPatterns = yield parseFilterPatterns(filterPatternsInput);
-        core.debug(`Change Filters - ${filterPatterns}`);
+        const filterPatternsStr = Object.entries(filterPatterns)
+            .map(s => s.join(':'))
+            .join(',');
+        core.debug(`Change Filters - ${filterPatternsStr}`);
         const changeMapInput = core.getInput('change-map');
+        core.debug(`Change Map Input - ${changeMapInput}`);
         const changeMap = yield parseChangeMapInput(changeMapInput);
-        core.debug(`Change Map - ${changeMap}`);
         return { changeMap, filterPatterns, fileChangeFindCommand };
     });
 }
